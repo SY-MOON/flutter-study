c0016a8e965df538749b690a4b3b320bf057c4fd
문석영(Seokyoung Moon)/Biz플랫폼개발팀/SKP <seokyoung.moon@sk.com>
2023-05-06 10:28:45 +0900

    ✨ 실습

---
+++ random_dice/lib/const/colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const backgroundColor = Color(0xFF0E0E0E);
+const primaryColor = Colors.white;
+final secondaryColor = Colors.grey[600]; // 런타임 색상계산으로 const 못씀!
+
